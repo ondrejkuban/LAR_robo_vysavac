e116e629d78f1cf32a552396a72abbeea15b60ba
Ondřej Kuban <qeerdy@gmail.com>
2022-03-06 16:32:15 +0100

    tried to estimate distance

--- test_bench.py
+++ test_bench.py
@@ -1,26 +1,37 @@
-
 import cv2
 import scipy.io
 import numpy as np
 
-mat = scipy.io.loadmat('2022-03-03-15-30-32.mat')
+# tresholds
+dark_blue = (90, 100, 20)
+light_blue = (110, 255, 255)
+dark_red = (0, 100, 50)
+light_red = (5, 255, 255)
+dark_green = (45, 70, 20)
+light_green = (75, 255, 255)
+#init
+mat = scipy.io.loadmat('2022-03-03-15-32-23.mat')
 rgb = mat['image_rgb']
-cv2.namedWindow("LOL")
+depth = mat['image_depth']
+cv2.namedWindow("RGB")
+cv2.namedWindow("DEPTH")
 while True:
-    hsv = cv2.cvtColor(rgb,cv2.COLOR_BGR2HSV)
-    dark_blue = (90, 100, 20)
-    light_blue = (110, 255, 255)
-    dark_red = (0, 100, 50)
-    light_red = (5, 255, 255)
-    dark_green = (45, 100, 20)
-    light_green = (75, 255, 255)
+
+    #convert to hsv
+    hsv = cv2.cvtColor(rgb, cv2.COLOR_BGR2HSV)
+
+    #creating mask to find rectangles
     maskb = cv2.inRange(hsv, dark_blue, light_blue)
-    maskr = cv2.inRange(hsv,dark_red,light_red)
+    maskr = cv2.inRange(hsv, dark_red, light_red)
     maskg = cv2.inRange(hsv, dark_green, light_green)
     result = cv2.bitwise_and(rgb, rgb, mask=maskb)
+
+    #finding rectangles
     outb = cv2.connectedComponentsWithStats(maskb.astype(np.uint8))
     outr = cv2.connectedComponentsWithStats(maskr.astype(np.uint8))
     outg = cv2.connectedComponentsWithStats(maskg.astype(np.uint8))
+
+    #finding biggest rectangles for each color
     max = 0
     max_b = []
     for i in range(1, outb[0]):
@@ -39,14 +50,21 @@ while True:
         if max < outg[2][i][4]:
             max = outg[2][i][4]
             max_g = outg[2][i]
-    print(max_g[0],max_g[1],max_g[0] + max_g[2],max_g[1]+max_g[3])
+    print(max_g[0], max_g[1], max_g[0] + max_g[2], max_g[1] + max_g[3])
 
+    #drawing rectangle
     im = rgb.copy()
-    cv2.rectangle(img=im, pt1=(max_b[0], max_b[1]), pt2=(max_b[0] + max_b[2], max_b[1] + max_b[3]), color=(0, 255, 0), thickness=2)
-    cv2.rectangle(img=im, pt1=(max_r[0], max_r[1]), pt2=(max_r[0] + max_r[2], max_r[1] + max_r[3]), color=(0, 0, 255),
-                  thickness=2)
-    cv2.rectangle(img=im, pt1=(max_g[0], max_g[1]), pt2=(max_g[0] + max_g[2], max_g[1] + max_g[3]), color=(255, 0, 255),
-                  thickness=2)
-
-    cv2.imshow("LOL",im)
+    cv2.rectangle(im, (max_b[0], max_b[1]), (max_b[0] + max_b[2], max_b[1] + max_b[3]), (0, 255, 0), 2)
+    cv2.rectangle(im, (max_r[0], max_r[1]), (max_r[0] + max_r[2], max_r[1] + max_r[3]), (0, 0, 255),
+                  2)
+    cv2.rectangle(im, (max_g[0], max_g[1]), (max_g[0] + max_g[2], max_g[1] + max_g[3]), (255, 0, 255),
+                  2)
+    #finding distance to green
+    dep = []
+    for i in range(max_g[0], max_g[2] + max_g[0]):
+        for j in range(max_g[1], max_g[3] + max_g[1]):
+            dep.append(depth[i][j])
+    cv2.putText(im, str(np.median(dep)), (max_g[0], max_g[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
+    cv2.imshow("RGB", im)
+    cv2.imshow("DEPTH", depth)
     cv2.waitKey(1)
