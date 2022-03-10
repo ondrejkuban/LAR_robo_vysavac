469daf631d14e12a73ded8f2dc3385908b93a7a7
Ondřej Kuban <qeerdy@gmail.com>
2022-03-10 17:41:05 +0100

    x

--- main.py
+++ main.py
@@ -185,7 +185,7 @@ def main():
             red_sort = sorted(blue_cones, key=lambda cone: cone.distance)
             if len(red_sort) > 1 and red_sort[0].angle is not None and red_sort[1].angle is not None:
                 if abs(abs(red_sort[0].angle) - abs(red_sort[1].angle)) > 0.09:
-                    print(abs(abs(red_sort[0].angle) - abs(red_sort[1].angle)))
+                    #print(abs(abs(red_sort[0].angle) - abs(red_sort[1].angle)))
                     if abs(red_sort[0].angle) > abs(red_sort[1].angle):
                         turtle.cmd_velocity(linear=0,
                                             angular=-pid.get_new_output(
