0465a83a28e1a988ba5a96a41d51de047f80fc2b
Ondřej Kuban <qeerdy@gmail.com>
2022-03-10 17:49:24 +0100

    x

--- main.py
+++ main.py
@@ -203,7 +203,7 @@ def main():
                 else:
                     turtle.cmd_velocity(linear=0.2, angular=0.0)
             elif len(red_sort) > 0 and red_sort[0].angle is not None:
-                if red_sort[0].center[0] > 320:
+                if red_sort[0].center[0] > 320 and red_sort[0].distance>0.15:
                     turtle.cmd_velocity(linear=0.0, angular=-0.2)
                 else:
                     turtle.cmd_velocity(linear=0.0, angular=0.2)
