451f3a97634c2c5a4a11835bdf422d979480f135
Ondřej Kuban <qeerdy@gmail.com>
2022-03-10 18:02:57 +0100

    x

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ def get_cones_for_color(image, threshold: tuple):
 def draw_rectangles(image, cones: list):
     for cone in cones:
         cv2.rectangle(image, cone.pt1, cone.pt2, color=get_threshold_for_color(cone.color), thickness=2)
-        cv2.putText(image, str(cone.x) + "  " + str(cone.y), cone.pt1, cv2.FONT_ITALIC, 1, get_threshold_for_color(cone.color), 2)
+        cv2.putText(image, str(cone.distance), cone.pt1, cv2.FONT_ITALIC, 1, get_threshold_for_color(cone.color), 2)
 
 
 def calculate_euclidean(first_point):  # points[2] for x and points[0] for y
@@ -190,7 +190,7 @@ def main():
                 else:
                     turtle.cmd_velocity(linear=0.2, angular=0.0)
             elif len(red_sort) > 0 and red_sort[0].angle is not None:
-                if red_sort[0].center[0] > 320 and red_sort[0].distance>0.25:
+                if red_sort[0].center[0] > 320 and red_sort[0].distance>0.35:
                     turtle.cmd_velocity(linear=0.0, angular=-0.2)
                 else:
                     turtle.cmd_velocity(linear=0.0, angular=0.2)
