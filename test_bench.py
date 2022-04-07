bba9cbb55713960ac8f2ea4b469ce4f1e284a5a8
Ondřej Kuban <qeerdy@gmail.com>
2022-04-07 14:47:17 +0200

    fixed closest pair func

--- test_bench.py
+++ test_bench.py
@@ -136,7 +136,8 @@ def get_threshold_for_color(color):
 
 # Return "array of Cones" from image
 def get_cones_for_color(image, threshold):
-    mask = cv2.inRange(image, tuple(map(tuple, threshold[0])), tuple(map(tuple, threshold[1])))
+    print(threshold[0])
+    mask = cv2.inRange(image,  threshold[0],  threshold[1])
     detections = cv2.connectedComponentsWithStats(mask.astype(np.uint8))
 
     results = []
