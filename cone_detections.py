2c974cc3bc215de277ac1ff41e76d2bb7aa0a1cc
Ondřej Kuban <qeerdy@gmail.com>
2022-04-18 14:57:00 +0200

    changed angle threshold

--- cone_detections.py
+++ cone_detections.py
@@ -173,4 +173,4 @@ def get_point_in_space(point_cloud, cone, axis, mask):
                         points.append(point_cloud[p][j][i][axis])
     # if axis == 2 and len(points)>0:
     ##print("points",points[0],points[-1])
-    return round(np.median(points), 3)
+    return round(np.median(np.percentile(points,20)), 3)
